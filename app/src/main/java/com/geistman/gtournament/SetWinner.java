2093131f76bbfc1dd298a296991a96d841958aec
geistman <geistman@gmail.com>
2016-02-27 22:10:22 +0100

    Buttons to choose winner have the player's name

--- app/src/main/java/com/geistman/gtournament/SetWinner.java
+++ app/src/main/java/com/geistman/gtournament/SetWinner.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 
 public class SetWinner extends AppCompatActivity {
 
@@ -18,5 +19,13 @@ public class SetWinner extends AppCompatActivity {
         Intent intent = getIntent();
         game = intent.getParcelableExtra(ChoosePlayer.GAME);
         Log.d(TAG, game.toString());
+
+        Button player1 = (Button) findViewById(R.id.player1);
+        player1.setText(game.getPlayer1());
+
+        Button player2 = (Button) findViewById(R.id.player2);
+        player2.setText(game.getPlayer2());
+
+
     }
 }
