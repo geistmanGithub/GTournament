bf14d49da3f57aac7368ed3f2ceb3561e0cfa0cb
geistman <geistman@gmail.com>
2016-02-28 14:54:09 +0100

    #5 Disable loser, but leaves cancel game active (that's a bug, but it's better that way)

--- app/src/main/java/com/geistman/gtournament/SetWinner.java
+++ app/src/main/java/com/geistman/gtournament/SetWinner.java
@@ -5,14 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
+
+import java.util.Objects;
 
 import static android.view.View.*;
 
 public class SetWinner extends AppCompatActivity {
 
     private static final String TAG = "Set Winner";
+    private static final String PLAYER1_TAG = "PLAYER1";
+    private static final String PLAYER2_TAG = "PLAYER2";
+
+
     private Game game;
 
     @Override
@@ -26,6 +32,7 @@ public class SetWinner extends AppCompatActivity {
 
         final Button player1Button = (Button) findViewById(R.id.player1);
         player1Button.setText(game.getPlayer1());
+        player1Button.setTag(PLAYER1_TAG);
         player1Button.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,6 +50,7 @@ public class SetWinner extends AppCompatActivity {
 
         final Button player2Button = (Button) findViewById(R.id.player2);
         player2Button.setText(game.getPlayer2());
+        player2Button.setTag(PLAYER2_TAG);
         player2Button.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -52,8 +60,23 @@ public class SetWinner extends AppCompatActivity {
         });
     }
 
-    private void getConfirmationForWinner(Button button) {
-        game.setWinner((String) button.getText());
+    private void getConfirmationForWinner(Button winnerButton) {
+        game.setWinner((String) winnerButton.getText());
+        ViewGroup parentView = (ViewGroup) findViewById(R.id.playerViewGroup);
+        setEnabledOfAllElementsOfViewExcept(parentView, winnerButton, false);
+    }
+
+    private void setEnabledOfAllElementsOfViewExcept(ViewGroup thisViewGroup, Button exceptThisButton, boolean enOrDisable) {
+        Object exceptThisButtonTag = exceptThisButton.getTag();
+
+        for (int i = 0; i < thisViewGroup.getChildCount(); i++){
+            View child = thisViewGroup.getChildAt(i);
+            Object childTag = child.getTag();
+            if (childTag == null || !childTag.equals(exceptThisButtonTag))
+                child.setEnabled(enOrDisable);
+            else
+                child.setEnabled(!enOrDisable);
+        }
     }
 
     private void startNewGame() {
